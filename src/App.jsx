9d094c580822acc4cf21fd1cf4ce9075bef8c660
Abrahan Mendoza <mendozaabrahan@gmail.com>
2021-05-06 19:40:49 +0200

    [[FEAT]] Added base styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import AppRouter from './routers/app_router';
+import './styles/styles.scss';
 
 const App = () => {
   return (
